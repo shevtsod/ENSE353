0cf5397830c4cafc38352872a29bf3f25e0dc3b8
shevtsod <daniel.shevtsov@gmail.com>
2017-06-02 22:40:50 -0600

    Modify index, add CSS, add temporary favicons

--- index.php
+++ index.php
@@ -2,16 +2,25 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+    <meta name="theme-color" content="#000000">
     <title>Daniel Shevtsov - ENSE353</title>
-    <link rel="stylesheet" src="css/normalize.css">
+    <!-- Roboto Font by Google Fonts (https://fonts.google.com/specimen/Roboto) -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-    <h1>Daniel Shevtsov Homepage</h1>
-    <h3>PHP Version: <?= phpversion(); ?></h3>
-    <p1>
+    <h1 id="title">Daniel Shevtsov</h1>
+    <p>
         This is my homepage<br>
-        Test
-    </p1>
+        PHP Version: <span id="phpver"><?= phpversion(); ?></span>
+    </p>
+
+    <div id="links">
+        <h2>Links</h2>
+        <a href="https://github.com/shevtsod">GitHub</a>
+        <!-- <span> | </span> -->
+    </div>
 </body>
 </html>
-
