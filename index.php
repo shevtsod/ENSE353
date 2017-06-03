6faa171f46596dfd547b52d95a93388ae3fe5e51
shevtsod <daniel.shevtsov@gmail.com>
2017-06-02 19:37:32 -0600

    Modify index

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Daniel Shevtsov - ENSE353</title>
-    <!-- <link rel="stylesheet" src="css/normalize.css"> -->
+    <link rel="stylesheet" src="css/normalize.css">
 </head>
 <body>
     <h1>Daniel Shevtsov Homepage</h1>
     <h3>PHP Version: <?= phpversion(); ?></h3>
     <p1>
         This is my homepage<br>
+        Test
     </p1>
 </body>
 </html>
+
